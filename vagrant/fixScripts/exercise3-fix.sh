489d51b0b3d449749b2d68b4f918d74e6d347635
Sprunknwn <haviner@gmail.com>
2019-02-05 19:16:27 +0200

    exercise1-fix.sh

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-#add fix to exercise3 here
+sudo sed -i 's/Deny from all/Allow from all/g;s/deny from all/Allow from all/g' /etc/apache2/sites-available/default
+
+sudo service apache2 restart
+
